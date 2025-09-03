2ce43fb51063de198ee2ca8818a56177a3b23110
jbhampton1 <126513556+JoBHampton@users.noreply.github.com>
2025-09-03 14:23:50 -0500

    added base configs
    the website should say "welcome to Big Spoon Yum" now

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../index.css'
+
+export const Home = () => {
+    return( 
+    <div>
+        <h1> Welcome to Big Spoon Yum</h1>
+        <p> for all your dietary needs</p>
+
+    </div>
+    );
+}
