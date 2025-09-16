947638da5e1eb30b3a6d53aaee68f04bc52053b2
jbhampton1 <126513556+JoBHampton@users.noreply.github.com>
2025-09-16 15:10:57 -0500

    LandingPage
    Added Landing Page for logged out users

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import '../index.css'
 import { useAuth0 } from "@auth0/auth0-react";
+import { LandingPage } from './LandingPage';
 
   
 
@@ -11,10 +12,7 @@ export const Home = () => {
     if(!isAuthenticated){
         return (
             <div>
-                <h1> Welcome to Big Spoon Yum</h1>
-                <p> for all your dietary needs</p>
-                
-                <button onClick={() => loginWithRedirect()}>Log In</button>
+                <LandingPage/>
 
             </div>
         );
